90aff265a2e87fd3baa07d9ee96501ff76c9db35
alilio313 <lioali6140@gmail.com>
2024-08-07 23:14:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from asBASE import asJSON
+
+db = asJSON("as.json")
+###
+
+
+SUDORS = [232499688] # ايديات المطورين
+API_ID = int(os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+TOKEN = os.environ.get("TG_BOT_TOKEN")
+bot = Client("control",API_ID,API_HASH,bot_token=TOKEN,in_memory=True)
+bot_id = TOKEN.split(":")[0]
