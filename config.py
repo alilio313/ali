70c374aca5a1436246c4d1fb7e93957d7babcb57
alilio313 <lioali6140@gmail.com>
2024-08-08 04:24:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ db = asJSON("as.json")
 
 
 SUDORS = [232499688] # ايديات المطورين
-API_ID = int(os.environ.get("API_ID")
+API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 TOKEN = os.environ.get("TG_BOT_TOKEN")
 bot = Client("control",API_ID,API_HASH,bot_token=TOKEN,in_memory=True)
